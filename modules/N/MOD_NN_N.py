29ad7cdcc71ab2478fb52b7f8282782d2945b1c4
pops-code <hohrakopetr@gmail.com>
2025-10-31 03:47:19 +0300

    Update MOD_NN_N.py

--- modules/N/MOD_NN_N.py
+++ modules/N/MOD_NN_N.py
@@ -2,7 +2,8 @@
 
 from modules.N.N_NUM import NNum
 from modules.N.DIV_NN_N import DIV_NN_N_f
-from modules.N.SUB_NDN_N import SUB_NDN_N_f
+from modules.N.SUB_NN_N import SUB_NN_N_f
+from modules.N.MUL_NN_N import MUL_NN_N_f
 from modules.N.NZER_N_B import NZER_N_B_f
 from modules.N.COM_NN_D import COM_NN_D_f
 
@@ -16,7 +17,7 @@ def MOD_NN_N_f(a: NNum, b: NNum) -> NNum:
 
     #COM_NN_D
     #Сравнение натуральных чисел: 2 - если первое больше второго; 0, если равно; 1 иначе.
-    if(COM_NN_D_f(a,b) not in [0,2]):
+    if (COM_NN_D_f(a,b) not in [0,2]):
         a, b = b, a
 
     #NZER_N_B
@@ -28,8 +29,18 @@ def MOD_NN_N_f(a: NNum, b: NNum) -> NNum:
     #Неполное частное от деления первого натурального числа на второе с остатком (делитель отличен от нуля)
     quotient = DIV_NN_N_f(a, b)
 
-    #SUB_NDN_N
-    #Вычитание из натурального другого натурального, умноженного на цифру для случая с неотрицательным результатом
-    remainder = SUB_NDN_N_f(a,b,quotient)
+    if (type(quotient) != NNum):
+        raise TypeError("Неполное частное не является натуральным числом")
+
+    # MUL_NN_N
+    # Умножение натуральных чисел
+    multi = MUL_NN_N_f(b,quotient)
+
+    if (COM_NN_D_f(a,multi) not in [0,2]):
+        raise ValueError("Теоретически невозможно, но а < b*q лол")
+
+    # SUB_NN_N
+    # Вычитание из первого большего натурального числа второго меньшего или равного
+    remainder = SUB_NN_N_f(a,multi)
 
     return NNum(remainder.n, remainder.A)
