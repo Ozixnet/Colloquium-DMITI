8560ca95a42b41a3207aa4667f21a6089fa2391e
TarasovYuri <bybilda21@gmail.com>
2025-11-02 23:32:40 +0300

    MOD_ZZ_Z.py

--- MOD_ZZ_Z.py
+++ MOD_ZZ_Z.py
@@ -1,5 +1,5 @@
 from modules.Z.Z_NUM import ZNum
-from modules.N.N_NUM import Nnum
+from modules.N.N_NUM import NNum
 from modules.Z.SUB_ZZ_Z import SUB_ZZ_Z
 from modules.Z.MUL_ZZ_Z import MUL_ZZ_Z
 from modules.Z.DIV_ZZ_Z import DIV_ZZ_Z
@@ -14,14 +14,16 @@ def MOD_ZZ_Z(arg1: ZNum, arg2:ZNum)-> ZNum:
                arg2 - делитель, целое число, не может быть 0
                Возврат - Znum
                """
-    if arg2 == ZNum(0, Nnum(1, [0])):
-        raise ValueError("ОШИБКА: Делитель должен быть отличен от нуля")
+    if arg2.n == 1 and arg2.A[0] == 0:
+        raise ValueError("Ошибка. Делитель равен нулю")
     # если делимое является 0
-    if arg1 == ZNum(0, Nnum(1, [0])):
-        return ZNum(0, Nnum(1, [0]))
+    if arg1.n == 1 and arg1.A[0] == 0:
+        return ZNum(0, NNum(1, [0]))
     # найдем частное от деления
     k = DIV_ZZ_Z(arg1, arg2)
     # умножим 2 аргумент на частное
     arg2 = MUL_ZZ_Z(arg2, k)
     # получаем и возвращаем остаток
     return SUB_ZZ_Z(arg1, arg2)
+
+
