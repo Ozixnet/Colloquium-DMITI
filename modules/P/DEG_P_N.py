2d260a08b4fc943d585548a6d633b307a03d40b1
pops-code <hohrakopetr@gmail.com>
2025-11-09 04:16:03 +0300

    Update DEG_P_N.py
    INT -> NNUM

--- modules/P/DEG_P_N.py
+++ modules/P/DEG_P_N.py
@@ -3,16 +3,17 @@
 from modules.P.P_NUM import PNum
 
 
-def DEG_P_N_f(p : PNum)->int:
+def DEG_P_N_f(p : PNum)->NNum:
     """
     Возвращает степень многочлена
 
     p - многочлен (типа PNum)
 
-    Возврат - int.
+    Возврат - NNum.
     """
     if p.m in [-1,0] and p.C[0].num_tor.A[-1] >= 0:
-        return 0
+        return NNum(1,[0])
 
     else:
-        return p.m
+        N = list(map(int,str(p.m).split()))[::-1]
+        return NNum(len(N), N)
