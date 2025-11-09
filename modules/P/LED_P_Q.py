f2ecbbd18073f52eff5949b5c1a0605451ea3ed5
pops-code <hohrakopetr@gmail.com>
2025-11-09 04:12:22 +0300

    var.5
    зафикшено

---
+++ modules/P/LED_P_Q.py
@@ -0,0 +1,15 @@
+# Хохряков Пётр, гр. 4381
+
+from modules.P.P_NUM import PNum
+from modules.Q.Q_NUM import QNum
+
+def LED_P_Q_f(p : PNum)->QNum:
+    """
+    Возвращает старший коэффициент многочлена
+
+    p - многочлен (типа PNum)
+
+    Возврат — QNum.
+    """
+
+    return p.C[-1]
