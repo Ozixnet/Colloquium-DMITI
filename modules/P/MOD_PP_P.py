194ce333e35b427ac389c8c4b7170d593cccc9d7
TarasovYuri <bybilda21@gmail.com>
2025-11-10 20:21:37 +0300

    Update MOD_PP_P.py

--- modules/P/MOD_PP_P.py
+++ modules/P/MOD_PP_P.py
@@ -3,21 +3,20 @@ from modules.P.DIV_PP_P import DIV_PP_P_f
 from modules.P.MUL_PP_P import MUL_PP_P_f
 from modules.P.SUB_PP_P import SUB_PP_P_f
 
+#Выполнил Тарасов Юрий 4381.
 
-# выполнил Тарасов Юрий гр.4381
 def MOD_PP_P_f(arg1: PNum, arg2: PNum) -> PNum:
     """
-                      Остаток от деления многочлена на многочлен при делении с остатком
-                      arg1 - первый многочлен
-                      arg2 - второй многочлен, не может быть 0
-                      Возврат - PNum
+    Остаток от деления многочлена на многочлен при делении с остатком
+    arg1 - первый многочлен.
+    arg2 - второй многочлен, не может быть 0.
+    Возврат - PNum.
     """
     if arg2.m == -1:
         raise ValueError("Ошибка. Делитель равен нулю")
-    # получим частное
+
+    # Получим частное
     quotient = DIV_PP_P_f(arg1, arg2)
-    #умножаем частное на делитель
-    product = MUL_PP_P_f(arg2, quotient)
-    # получаем остаток
+    # Получаем остаток
     res = SUB_PP_P_f(arg1, MUL_PP_P_f(arg2, quotient))
     return res
